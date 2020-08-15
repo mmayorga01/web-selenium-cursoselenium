f1db9248975f9d99efe2dd05634b8d9cd9059962
Gabby <gabby.mayorgas@gmail.com>
2020-08-15 12:03:24 -0500

    Second Commit

---
+++ SeleniumAutomation/src/test/java/com/javabasics/Datatypes.java
@@ -0,0 +1,51 @@
+package com.javabasics;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Datos Numéricos
+//      byte x;
+//      short y;
+//      int z;
+//      long a;
+//      
+//      x=127;
+//      y=32767;
+//      z=2147483647;
+//      a=9223372036854775807L;//Colocar la L
+//      
+//      System.out.println(x);
+//      System.out.println(y);
+		
+		//Datos numéricos reales
+//		float base;//Siempre colocar la F
+//		double altura;
+//		
+//		base=30.5F;
+//		altura=40.76;
+//				
+//		
+//		  System.out.println(base);
+		
+			//Datos de Caracteres
+//			char letra1;
+//			char letra2;
+//			String frase;
+//			
+//		letra1='a';
+//		letra2='\u0047';
+//		frase="Hola";
+//			
+//			System.out.println(frase);
+//			System.out.println(letra2);
+		
+		//Datos boleanos
+		
+		boolean avesVuelan;
+		avesVuelan=true;
+		System.out.println("las aves vuelan");
+		
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasics/OperatorsStatements.java
@@ -0,0 +1,38 @@
+package com.javabasics;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Operadores Aritméticos
+//		int x=1;
+//		int y=2;
+//		int suma;
+//		
+//		suma =x+y;
+//		
+//		System.out.println(suma);
+		
+		//Operadores Unarios
+//		int valor1=3;
+//		int incrementar = ++valor1;
+//		System.out.println(incrementar);
+	
+	//Operadores Relacionales
+	int num1=10;
+	int num2=4;
+	int num3=6;
+	int num4=11;
+	if(num1 >=10 && num2>=10) {
+		
+		System.out.println("los número son mayores a 10");
+	}
+		
+		else {
+			System.out.println("los números son menores a 10");
+
+	
+	}	
+	}
+
+}
