e70ae3a911e8f87a76d5fe9579fc6b04a49674a5
Gabby <gabby.mayorgas@gmail.com>
2020-08-31 17:03:35 -0500

    Five Commit

--- SeleniumAutomation/src/test/java/com/exercise1/PolimorfismoPoligono.java
+++ SeleniumAutomation/src/test/java/com/exercise1/PolimorfismoPoligono.java
@@ -13,7 +13,7 @@ public class PolimorfismoPoligono {
 		double area1 = poligono1.area();
 		System.out.println("El area de este poligono es: "+area1);
 
-		
+		//Triangulo
 		poligono1=new Triangulo(3,3,3);
 		System.out.println(poligono1.toString());
 		double perimetro2 = poligono1.perimetro();
@@ -21,6 +21,16 @@ public class PolimorfismoPoligono {
 		poligono1=new Triangulo(2,2);
 		double area2 = poligono1.area();
 		System.out.println("El area de este poligono es: "+area2);
+		
+		
+		//Trapecio
+		poligono1=new Trapecio(3,4,3,5);
+		System.out.println(poligono1.toString());
+		double perimetro3 = poligono1.perimetro();
+		System.out.println("El perimetro de esta figura es:"+perimetro3);
+		poligono1=new Trapecio(5,4,2.8);
+		double area3 = poligono1.area();
+		System.out.println("El area de este poligono es: "+area3);
 	}
 
 }
